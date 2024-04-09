8432a6b319b40e602c164f836170f4c390b8c827
miraz <fmsmiraz@gmail.com>
2024-04-10 02:01:28 +0600

    news added & navbar updated

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,61 +1,71 @@
-<div class = "top-bar d-flex flex-d-row justify-around">
-  <div class="col-md-4 col-sm-3 left px-4 py-2 welcome-msg"> 
-    <p>Sweet Tooth  | A top branded Sweet shop</p>
-  </div>
-  <form class="d-flex col-md-3 col-sm-3 ml-4 mt-2" role="search">
-    <input class="form-control me-2 " type="search" placeholder="Search" aria-label="Search">
-    <button type="button" class="btn btn-outline-warning">Search</button>
-  </form>
-  <div class="px-10 mt-1">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary ml-10">
-      <ul class="navbar-nav">
+<div class = "top-bar w-2 d-flex flex-d-row justify-around">
+    <div class="col-md-4 col-sm-3 left mt-2 px-4 py-2 welcome-msg"> 
+      <p>Sweet Tooth  | A top branded Sweet Shop</p>
+    </div>
+    <div class="headerSearch mt-2 mr-1.5 me-5">
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+    
+  <div class="topNav mt-1 ml-1.5 px-10 ">
+    <nav class="navbar-top">
+      <ul class="navbarTopUl">
           @auth
             <li class="nav-item">
-              <a class="nav-link" href="{{route('logout')}}">Log-out</a>
+              <a class="nav-link" href="{{route('logout')}}">Log-out   </a>
             </li>
             @else
-            <li class="nav-item">
-              <a class="nav-link" href="{{route('login')}}">Log-in</a>
+            <li class="nav-item ml-2">
+              <a class="nav-link" href="{{route('login')}}">Log-in   </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="{{route('registration')}}">Registration</a>
+            <li class="nav-item ml-2">
+              <a class="nav-link" href="{{route('registration')}}">Registration   </a>
             </li>
             @endauth
           </ul>
-          @auth
-          {{auth()->user()->name}}
-          @endauth
+          <div style="margin-left: 5px">
+            @auth
+            {{auth()->user()->name}}
+            @endauth
+          </div>
+          
         </ul>
     </nav>
-  </div>
+  </div> 
 </div>
-<div class = "bottom-bar">
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
+<div class = "bottom-bar" style="background-color: #77d8f0; height:100px">
+  <div class="bottom-bar-pic">
+    <img id="image" src="{{asset('images/sweet2.jpeg')}}" alt="logo" class="logo" height="80px" width="80px">
+  </div>
+  <div class="bottomNavDiv" style="background-color: #77d8f0">
+  <nav class="navbar-bottom" style="background-color: #f59494;">
+        <ul class="navbar-nav" style="background-color: #f59494;">
+          <li class="nav-item" style="color: white">
+            <a class="nav-link active" aria-current="page" href="{{route('home')}}"><h4>Home</h4></a>
           </li>
-          @auth
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('logout')}}">Log-out</a>
+          <li class="nav-item" style="color: white">
+            <a class="nav-link active" aria-current="page" href="{{route('home')}}"><h4>Products</h4></a>
           </li>
-          @else
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('login')}}">Log-in</a>
+          <li class="nav-item" style="color: white">
+            <a class="nav-link active" aria-current="page" href="{{route('home')}}"><h4>Showroom</h4></a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('registration')}}">Registration</a>
+          <li class="nav-item" style="color: white">
+            <a class="nav-link active" aria-current="page" href="{{route('home')}}"><h4>About Us</h4></a>
           </li>
-          @endauth
+          <li class="nav-item" style="color: white">
+            <a class="nav-link active" aria-current="page" href="{{route('home')}}"><h4>Event</h4></a>
+          </li>
+          <li class="nav-item" style="color: white">
+            <a class="nav-link active" aria-current="page" href="{{route('home')}}"><h4>Gallery</h4></a>
+          </li>
+          <li class="nav-item" style="color: white">
+            <a class="nav-link active" aria-current="page" href="{{route('home')}}"><h4>Contact</h4></a>
+          </li>
         </ul>
-        @auth
-        {{auth()->user()->name}}
-        @endauth
-      </div>
-    </div>
   </nav>
+</div>
 </div>
 
   {{-- <nav class="position-fixed navbar navbar-expand-lg bg-body-tertiary">
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,9 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'custom auth laravel')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <link rel="stylesheet" href="{{asset('css/header.css')}}">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/news.css" />
   </head>
   <body>
     @include('include.header')
+    @include('include.news')
     @yield('content')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" ></script>
   </body>
